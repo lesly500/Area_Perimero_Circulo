56bf56b8435ab5aa4e908f4c104c1c6f749aecd7
lesly500 <leslyhernandez@colegioguanenta.edu.co>
2025-01-29 07:21:18 -0500

    se agrego codigo pathin

---
+++ area_perimetro_circulo.py
@@ -0,0 +1,23 @@
+import math
+
+print("··························")
+print("Area perimetro del circulo")
+print("··························")
+
+
+# input
+r = input("Digite el valor del radio: ")
+r = int(r)
+
+# processing
+p = 2*math.pi*r
+a = math.pi*r**2
+
+# output
+print("··························")
+print("El area es: "+ str(a))
+print("El perimetro es: " + str(p))
+print("··························")
+
+
+
